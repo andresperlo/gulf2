3e11bb6102dc051a66a8df8820288c46d6d8b39c
unknown <andres_perlo@gmail.com>
2020-09-23 10:57:02 -0300

    se hace cambios modernos

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -9,6 +9,8 @@ import humita from '../img/humita.jpg'
 import tamales from '../img/tamales.jpg'
 import asado from '../img/asado.jpg'
 import sandwich from '../img/sandwich.jpg'
+import sandwichlomito from '../img/sandwichlomito.jpg'
+import sandwichternerayqueso from '../img/ternerayqueso.jpg'
 import napo from '../img/napo.jpg'
 import cafe from '../img/cafe.jpeg'
 import cafeconleche from '../img/cafeconleche.jpg'
@@ -16,6 +18,18 @@ import cortadito from '../img/cortadito.jpg'
 import cerveza from '../img/cerveza.jpg'
 import gaseosas from '../img/gaseosas.jpg'
 import aguas from '../img/aguasaborizada.jpg'
+import home from '../img/home.jpg'
+import lubricantes from '../img/lubricantes.jpg'
+import frente1 from '../img/frente1.jpg'
+import adelante from '../img/adelante.jpg'
+import patio from '../img/patio.jpg'
+import kiosco from '../img/kiosco.jpg'
+import light from '../img/light.jpg'
+import licuado from '../img/licuado.jpg'
+import te from '../img/te.jpg'
+import quilmes from '../img/quilmes.jpg'
+import fresh from '../img/fresh.jpg'
+import neon from '../img/neon.jpg'
 
 
 function HomePages() {
@@ -25,30 +39,31 @@ function HomePages() {
                 {/* Autor: Andres Perlo - CEl 3814443123 */}
 
                 <div id="carouselExampleFade" class="div-Carousel carousel slide carousel-fade" data-ride="carousel">
-                    <div className='div-gulf'>
-                        <h1>Gulf Tucuman</h1>
-                        <h3>Restobar & Drugstore</h3>
-                        <h5>Comidas, Bebidas y Golosinas</h5>
-                    </div>
+
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src={img1} class="d-block w-100 class-img-css" alt="..." />
+                            <img src={frente1} class="d-block w-100 class-img-css" alt="..." />
                         </div>
                         <div class="carousel-item">
-                            <img src={img2} class="d-block w-100 class-img-css" alt="..." />
+                            <img src={adelante} class="d-block w-100 class-img-css" alt="..." />
                         </div>
                         <div class="carousel-item">
-                            <img src={img3} class="d-block w-100 class-img-css" alt="..." />
+                            <img src={patio} class="d-block w-100 class-img-css" alt="..." />
+                        </div>
+                        <div class="carousel-item">
+                            <img src={kiosco} class="d-block w-100 class-img-css" alt="..." />
                         </div>
                     </div>
+                    <div className='neon-css'>
+                        <img src={neon} className='imagen-neon-css' alt="" />
+                    </div>
                     <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-
                     </a>
                     <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-
                     </a>
                 </div>
             </div>
+
             <div className='pt-5 pb-3 text-center'>
                 <h3>Nuestro Menu</h3>
             </div>
@@ -56,73 +71,87 @@ function HomePages() {
                 <h2 className='pb-3 h2-css'>Cafeteria</h2>
                 <div className="card-columns">
                     <div className="card border border-dark">
-                        <img src={cafe} className="card-img-top border-bottom border-dark" alt="..." />
+                        <img src={cafe} className="card-img-top border-bottom border-dark img-css-cafeteria" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Cafe Negro</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title text-center">Cafe Negro</h5>
                         </div>
                     </div>
                     <div className="card border border-dark">
-                        <img src={cafeconleche} className="card-img-top border-bottom border-dark" alt="..." />
+                        <img src={cortadito} className="card-img-top border-bottom border-dark img-css-cafeteria" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Cafe Con Leche</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title text-center">Cortadito</h5>
                         </div>
                     </div>
                     <div className="card border border-dark">
-                        <img src={cortadito} className="card-img-top border-bottom border-dark" alt="..." />
+                        <img src={cafeconleche} className="card-img-top border-bottom border-dark img-css-cafeteria" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Cortadito</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title text-center">Cafe Con Leche</h5>
+                        </div>
+                    </div>
+                    <div className="card border border-dark">
+                        <img src={licuado} className="card-img-top border-bottom border-dark img-css-cafeteria" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Licuado + Tostado Mixto</h5>
+                        </div>
+                    </div>
+                    <div className="card border border-dark">
+                        <img src={light} className="card-img-top border-bottom border-dark img-css-cafeteria" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Desayuno / Merienda Light</h5>
+                        </div>
+                    </div>
+                    <div className="card border border-dark">
+                        <img src={te} className="card-img-top border-bottom border-dark img-css-cafeteria" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Te o Mate Cocido</h5>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div className="container pb-3">
                 <h2 className='py-3 h2-css'>Cocina</h2>
                 <div className="card-columns">
                     <div>
-                        <div className="card border border-dark">
-                            <img src={empanadas} className="card-img-top border-bottom border-dark" alt="..." />
+                        <div className="card border border-dark cards-css">
+                            <img src={empanadas} className="card-img-top border-bottom border-dark img-css-cocina" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">Empanadas Tucumanas</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title text-center">Empanadas Tucumanas de la Campeona</h5>
                             </div>
                         </div>
-                        <div className="card border border-dark">
-                            <img src={asado} className="card-img-top border-bottom border-dark" alt="..." />
+                        <div className="card border border-dark cards-css">
+                            <img src={asado} className="card-img-top border-bottom border-dark img-css-cocina" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">Asado Tradicional</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title text-center">Asado Tradicional</h5>
                             </div>
                         </div>
-                        <div className="card border border-dark">
-                            <img src={humita} className="card-img-top border-bottom border-dark" alt="..." />
+                        <div className="card border border-dark cards-css">
+                            <img src={napo} className="card-img-top border-bottom border-dark img-css-cocina" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">Humita al Plato</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title text-center">Napolitana Para Dos</h5>
                             </div>
                         </div>
-                        <div className="card border border-dark">
-                            <img src={sandwich} className="card-img-top border-bottom border-dark" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Sandwich</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="card-columns">
+                    <div className="card border border-dark cards-css">
+                        <img src={sandwich} className="card-img-top border-bottom border-dark img-css-cocina" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Sandwich</h5>
                         </div>
-                        <div className="card border border-dark">
-                            <img src={tamales} className="card-img-top border-bottom border-dark" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Tamales Tucumanos</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                    </div>
+                    <div className="card border border-dark cards-css">
+                        <img src={sandwichlomito} className="card-img-top border-bottom border-dark img-css-cocina" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Sandwich de Lomito</h5>
                         </div>
-                        <div className="card border border-dark">
-                            <img src={napo} className="card-img-top border-bottom border-dark" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Napolitana Para Dos</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                    </div>
+                    <div className="card border border-dark cards-css">
+                        <img src={sandwichternerayqueso} className="card-img-top border-bottom border-dark img-css-cocina" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Sandwich de Ternera y Queso</h5>
                         </div>
                     </div>
                 </div>
@@ -132,37 +161,42 @@ function HomePages() {
                 <div className="card-columns">
                     <div>
                         <div className="card border border-dark">
-                            <img src={cerveza} className="card-img-top border-bottom border-dark" alt="..." />
+                            <img src={quilmes} className="card-img-top border-bottom border-dark img-cards-gaseosas" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">Bebidas con Alcohol</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title text-center ">Bebidas con Alcohol</h5>
+                            </div>
+                        </div>
+                        <div className="card border border-dark ">
+                            <img src={gaseosas} className="card-img-top border-bottom border-dark img-cards-gaseosas" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Bebidas Sin Alcohol</h5>
                             </div>
                         </div>
                         <div className="card border border-dark">
-                            <img src={gaseosas} className="card-img-top border-bottom border-dark" alt="..." />
+                            <img src={fresh} className="card-img-top border-bottom border-dark img-cards-gaseosas" alt="..." />
                             <div className="card-body">
-                                <h5 className="card-title">Bebidas Sin Alcohol</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                        <div className="card border border-dark">
-                            <img src={aguas} className="card-img-top border-bottom border-dark" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Agua Saborizada</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 className="card-title text-center">Agua Saborizada</h5>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div>
-                <footer className='py-3 text-white footer-css'>
+                <footer className='text-white footer-css'>
                     <div className='ml5-css'>
                         <img src={logo} className='img-logo' alt="" />
                     </div>
                     <div className='text-center'>
-                        <h2>Hacer Pedidos:<br /> <i className="fas fa-phone-volume py-3"></i> 381 603 6303</h2>
-                        <h4><i className="fas fa-map-marker-alt"></i> Av. Siria 1968</h4>
+                        <h2><i className="fas fa-map-marker-alt pb-3 css-h2 icon-avsiria"></i> Av. Siria 1968</h2>
+                        <div className='d-flex justify-content-center'>
+                            <h2><i className="fas fa-phone-volume pb-3 icon-css"></i></h2>
+                            <h2><i class="fab fa-whatsapp icon-css mx-3"></i></h2>
+                            <h2><i class="fas fa-envelope icon-css"></i></h2>
+                        </div>
+                        <h2 className='css-h2'>381 603 6303</h2>
+                        <h2 className='tucuman-css'>San Miguel de Tucuman</h2>
+                        <h2 className='tucuman-css'>TUCUMAN - ARGENTINA</h2>
+
                     </div>
                     <div className='mr5-css text-center'>
                         <h4 className='pb-3'>Haz cick para ver como llegar</h4>
